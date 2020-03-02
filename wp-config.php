6c339d1edec4a246257d0f5132e291f0f46f1598
skorik-kirill <60388222+skorik-kirill@users.noreply.github.com>
2020-03-02 17:06:25 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,8 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME']);
+define('WP_HOME', 'http://' . $_SERVER['SERVER_NAME']);
 /**#@+
  * Authentication Unique Keys and Salts.
  *
